f9a52a2fdba8297d24dc48317f2ba777d29f68ef
Ckyle Joshua Ancheta <ckylejoshuaancheta03@gmail.com>
2021-01-25 20:51:10 +0800

    assign

---
+++ Assignmentas/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Assignmentas.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assignmentas
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Assignmentas/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Assignmentas.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public void LoginButton(String UserName)
+        {
+            MessageBox.Show(String.Format("Welcome {0}",UserName));
+        }
+    }
+}
